d3dd360735a14815d42389dfb6836970bd9edba1
Amrapalli Garanaik <amrapallig@lanl.gov>
2019-12-06 16:57:00 -0700

    updated with meshfle

--- ocean/lagrangian_particles/diffusivity_realization_data_lagrangian.py
+++ ocean/lagrangian_particles/diffusivity_realization_data_lagrangian.py
@@ -15,7 +15,7 @@
 import os
 import numpy as np
 import netCDF4
-import cPickle as pickle
+import _pickle as pickle
 
 # function definitions
 
@@ -81,7 +81,7 @@ def proj_xyz(plat, plong, r):  #{{{
     
     return x,y,z  #}}}
 
-def get_clusters(f_in, cluster_path): #{{{
+def get_clusters(m_in, cluster_path): #{{{
     """
     Compute or load the cluster information to link particles
     to clusters to compute dispersion.
@@ -91,14 +91,14 @@ def get_clusters(f_in, cluster_path): #{{{
     """
     # compute interior cells and masks for vertices 
     if cluster_path is None:
-      print 'Computing particle clusters'
-      verticesOnCell = f_in.variables['verticesOnCell']
-      nEdgesOnCell   = f_in.variables['nEdgesOnCell']
-      cellsOnVertex  = f_in.variables['cellsOnVertex'] 
+      print ('Computing particle clusters')
+      verticesOnCell = m_in.variables['verticesOnCell']
+      nEdgesOnCell   = m_in.variables['nEdgesOnCell']
+      cellsOnVertex  = m_in.variables['cellsOnVertex'] 
       boundaryVertex = (cellsOnVertex[:,:] == 0)
       boundaryVertex = boundaryVertex.max(axis=1)
       
-      nCells = len(f_in.dimensions['nCells'])
+      nCells = len(m_in.dimensions['nCells'])
       inCells = np.ones(nCells+1)
       inCells[cellsOnVertex[boundaryVertex,:]-1] = 0
       boundaryCells = (inCells == 0)
@@ -108,17 +108,17 @@ def get_clusters(f_in, cluster_path): #{{{
       verticesToParticle = np.cumsum(1-boundaryVertex) - 1
 
       # store cluster neighbor information for filter
-      print 'saving verticesOnCell, maxVertices, verticesToParticle'
+      print ('saving verticesOnCell, maxVertices, verticesToParticle')
       pickle.dump(verticesToParticle, open( "verticesToParticle.p", "wb"))
       pickle.dump(verticesOnCell, open( "verticesOnCell.p", "wb"))
       pickle.dump(maxVertices, open( "maxVertices.p", "wb"))
-      print 'done'
+      print ('done')
     else:
-      print 'laoding cluster info'
+      print ('laoding cluster info')
       verticesToParticle = pickle.load(open(cluster_path+"/verticesToParticle.p","rb"))
       verticesOnCell = pickle.load(open(cluster_path+"/verticesOnCell.p","rb"))
       maxVertices = pickle.load(open(cluster_path+"/maxVertices.p","rb"))
-      print 'done'
+      print ('done')
 
     return verticesOnCell, maxVertices, verticesToParticle #}}}
 
@@ -143,7 +143,7 @@ def compute_com_and_dispersion(plat, plon, r, indexOnCluster, maxIndex, indexToP
     drdr = np.zeros(N)
     Npart = np.zeros(N)
     
-    print 'Computing COM and dispersion for ', N, ' clusters'
+    print ('Computing COM and dispersion for ', N, ' clusters')
     for aCluster, maxInd in enumerate(maxIndex):
         # get points of cluster
         particles = indexToParticle[indexOnCluster[aCluster,0:maxInd]]
@@ -175,7 +175,7 @@ def compute_com_and_dispersion(plat, plon, r, indexOnCluster, maxIndex, indexToP
 
     return clon, clat, dxdx, dxdy, dydy, drdr, Npart #}}}
 
-def build_particle_file(fname_in, cluster_path):  #{{{
+def build_particle_file(fname_in, mname_in, cluster_path):  #{{{
     """
     Take existing netCDF4 input f_in and produce
     vtu files for f_outbase with all the particle data
@@ -186,9 +186,10 @@ def build_particle_file(fname_in, cluster_path):  #{{{
     """
     # load the netCDF database
     f_in = netCDF4.Dataset(fname_in, 'r')
+    m_in = netCDF4.Dataset(mname_in, 'r')
 
     # get relationship between a cluster and particles (group particles into clusters)
-    indexOnCluster, maxIndex, indexToParticle = get_clusters(f_in, cluster_path)
+    indexOnCluster, maxIndex, indexToParticle = get_clusters(m_in, cluster_path)
     
     Ntime = len(f_in.dimensions['Time'])
     Nclusters = maxIndex.shape[0]
@@ -203,7 +204,7 @@ def build_particle_file(fname_in, cluster_path):  #{{{
     Npart = np.zeros((Ntime,Nclusters))
 
     for t in np.arange(Ntime):
-        print 'Looping over step ', t, ' of ', Ntime
+        print ('Looping over step ', t, ' of ', Ntime)
       
         x = f_in.variables['xParticle'][t]
         y = f_in.variables['yParticle'][t]
@@ -215,11 +216,12 @@ def build_particle_file(fname_in, cluster_path):  #{{{
         mux[t,:], muy[t,:], dxdx_sum[t,:], dxdy_sum[t,:], dydy_sum[t,:], drdr_sum[t,:], Npart[t,:] = \
                 compute_com_and_dispersion(plat, plon, rEarth, indexOnCluster, maxIndex, indexToParticle)
    
-    print 'saving numpy arrays'
+    print ('saving numpy arrays')
     np.savez('lagrangian_data.npz',mux=mux, muy=muy, dxdx_sum=dxdx_sum, dxdy_sum=dxdy_sum, dydy_sum=dydy_sum, drdr_sum=drdr_sum, Npart=Npart)
-    print 'done'
+    print ('done')
 
     f_in.close()
+    m_in.close()
 
     return  #}}}
 
@@ -230,6 +232,13 @@ if __name__ == "__main__":
                       help="file to open for appending \
                       particle data 'particle_' extension",
                       metavar="FILE")
+    
+
+    parser.add_option("-m", "--mesh", dest="meshfilename",
+                      help="file to open for appending \
+                      mesh data 'mesh_' extension",
+                      metavar="FILE")
+
     parser.add_option("-c", "--load_cluser_info_path", dest="clusterinfopath",
                       help="location of files with cluster info", 
                       metavar="DIR")
@@ -238,7 +247,9 @@ if __name__ == "__main__":
 
     if not options.inputfilename:
         parser.error("Input filename is a required input.")
+    if not options.meshfilename:
+        parser.error("Mesh filename is a required input.")
     if not options.clusterinfopath:
         options.clusterinfopath = None
 
-    build_particle_file(options.inputfilename,options.meshfile, options.clusterinfopath)
+    build_particle_file(options.inputfilename,options.meshfilename, options.clusterinfopath)
